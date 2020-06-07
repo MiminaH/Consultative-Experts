677cdde455b128da1be021111b46e902adfdfb84
Mimina <yasmin.hamdy@gmail.com>
2020-06-07 23:16:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// jshint esversion:6
+
+// const sectionIds = ["#announcements", "#about-us", "#our-services", "#our-customers", "#contact-us" ];
+//
+// $('html,body').animate({
+//   scrollTop: $("#about-us").offset().top - 120
+// });
+
+
+
+/* TO-DO:
+    - make navabr responsive to mobile screens.
+    - style the scrolling bar on the right.
+    - add announcements section.
+    - add services details when clicking a service card.
+    - add elment appeearance animation upon loading the website or scrolling to a section.
+    - fix navigation bug when clicking a nav item to navigate to a section.
+    - put customer logos in a carousel that scrolls automotically once loaded.
+*/
