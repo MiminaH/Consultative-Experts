cb9faa381977878b3bb13ca74452e6836979f914
Mimina <yasmin.hamdy@gmail.com>
2020-06-15 03:20:03 +0200

    added services pages & redirecting function.

--- index.js
+++ index.js
@@ -54,53 +54,76 @@ function announcementTemplate(announcement) {
 
 $(".announcements_slider_wrapper").html(`${announcements.map(announcementTemplate).join("")}`);
 
+// Announcement Sider
 
-// OUR SERVICES SECTION ---------------------------------------
+// .ready() when the page is loaded do what's inside function
+// $(document).ready(() => {
+//
+// });
+
+// $('.slick').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   dots: true
+// });
+
+// OUR-SERVICES SECTION ---------------------------------------
 const services = [{
     "service": "تأسيس كافة أنواع الشركات وفروعها داخل وخارج مصر",
-    "icon": "images\\icons\\icons8-licence-100 (green).png"
+    "icon": "images\\icons\\icons8-licence-100 (green).png",
+    "file": "service_details\\1-establishing_companies.html"
   },
   {
     "service": "خدمات الاستثمار الصناعي",
-    "icon": "images\\icons\\icons8-factory-100 (brown).png"
+    "icon": "images\\icons\\icons8-factory-100 (brown).png",
+    "file": "service_details\\2-industrial_investment.html"
   },
   {
     "service": "الخدمات الضريبية والمحاسبية",
-    "icon": "images\\icons\\icons8-ledger-100 (orange).png"
+    "icon": "images\\icons\\icons8-ledger-100 (orange).png",
+    "file": "service_details\\3-tax_N_accounting.html"
   },
   {
     "service": "خدمات رجال الاعمال",
-    "icon": "images\\icons\\icons8-permanent-job-100 (cyan).png"
+    "icon": "images\\icons\\icons8-permanent-job-100 (cyan).png",
+    "file": "service_details\\4-business_men.html"
   },
   {
     "service": "خدمات الشركات",
-    "icon": "images\\icons\\icons8-business-building-100 (red).png"
+    "icon": "images\\icons\\icons8-business-building-100 (red).png",
+    "file": "service_details\\5-companies.html"
   },
   {
     "service": "خدمات الاجانب",
-    "icon": "images\\icons\\icons8-travel-visa-100 (yellow).png"
+    "icon": "images\\icons\\icons8-travel-visa-100 (yellow).png",
+    "file": "service_details\\6-foreigners.html"
   },
   {
     "service": "الاستشارات القانونية",
-    "icon": "images\\icons\\icons8-scales-100 (blue).png"
+    "icon": "images\\icons\\icons8-scales-100 (blue).png",
+    "file": "service_details\\7-legal_advice.html"
   },
   {
     "service": "محكمه الاسرة",
-    "icon": "images\\icons\\icons8-family-100 (purple).png"
+    "icon": "images\\icons\\icons8-family-100 (purple).png",
+    "file": "service_details\\8-family.html"
   }
 ];
 
 function serviceTemplate(service) {
-  return `<div class="service_card">
+  return `<a href="${service.file}">
+  <div class="service_card">
     <img class="service_icon" src="${service.icon}" alt="">
     <p>${service.service}</p>
-    </div>`;
+    </div>
+    </a>`;
 }
 
 $(".services_cards_div").html(`${services.map(serviceTemplate).join("")}`);
 
 
-// OUR CUSTOMERS SECTION -------------------------------------
+// OUR-CUSTOMERS SECTION -------------------------------------
 const customers = [{
     "image": "images\\customer logos\\Al-Amir.jpg"
   },
@@ -146,3 +169,50 @@ function customersTemplate(customer) {
 }
 
 $(".customers_cards_div").html(`${customers.map(customersTemplate).join("")}`);
+
+
+// DOUBLE NAVBAR (for services html) ---------------------------
+const doubleNavbarHTML = `<img class="logo_img" src="..\\images\\logo right-left (horizontal).jpg" alt="logo-img">
+
+<div class="nav_links_div">
+  <a href="#">الرئيسية</a>
+  <a href="#announcements">أنباء وأخبار</a>
+  <a href="#about-us">تعرف علينا</a>
+  <a href="#our-services">خدماتنا</a>
+  <a href="#our-customers">عملائنا</a>
+  <div class="contact_us_btn_div">
+    <a href="#contact-us">تواصل معنا</a>
+  </div>
+</div>`;
+
+$(".nav_for_services").html(doubleNavbarHTML);
+
+
+// CONTACT-US SECTION (for services html) ---------------------------
+const contactUsHTML = `<div class="contact_us_title_div">
+  <p class="contact-us_title">تواصل معنا</p>
+</div>
+
+<div class="contact-us_details_div">
+  <div class="contact-us_info_div">
+    <p><span>العنوان:</span> ٩ عمارات مترو سيتي, شارع الخمسيني, زهراء المعادي, المعادي, القاهرة</p>
+    <p><span>أرضي وفاكس:</span> ٠٢٢٧٣٤٠٥٤٥</p>
+    <p><span>موبيل:</span> ٠١٠٦٦٦٦٤٦٦٠ / <span>واتس:</span> ٠١١٤٠١٠٧٩٧٩</p>
+    <p><span>بريد إلكتروني:</span> Sherif.nassar@c-experts.com</p>
+  </div>
+  <div class="contact-us_map">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.4843052600336!2d31.314252084886707!3d29.965508681911036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145839ee07b964cb%3A0xb0226e309dcdf5a!2z2LTYsdmD2Kkg2KfZhNiu2KjYsdin2KEg2KfZhNil2LPYqti02KfYsdmK2YjZhg!5e0!3m2!1sar!2seg!4v1591781122894!5m2!1sar!2seg"
+      width="500" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </div>
+</div>`;
+
+$(".contact-us_section").html(contactUsHTML);
+
+// COPYRIGHT FOOTER (for services html) ---------------------------
+const copyrightHTML = `<div class="copyright_div">
+  <p class="copyright_p">© Consultative Experts 2020.</p>
+
+  <p class="created-by_p">Create by <a href="https://miminah.github.io/MiminaWebsite/tech.html">Mimina</a></p>
+</div>`;
+$(".footer_section").html(copyrightHTML);
